62c34068d55145354b6a39e225004a71ec16702a
Isaiah Tshabalala <lakeside.junction.1984@gmail.com>
2023-07-14 11:53:27 +0200

    Corrected the CSS to suit this application.

--- src/components/About.js
+++ src/components/About.js
@@ -1,26 +1,26 @@
 function About() {
     return (
-      <div className='mt-4 p-5 bg-primary text-white rounded'>
-        <h1 className='mb-4 mt-4'>Github Finder</h1>
+      <div className='m-3 text-white rounded'>
+        <h1 className='mb-4'>Github Finder</h1>
         <p className='mb-4 font-light'>
           A React app to search GitHub profiles and see profile details. This
           project is part of the
-          <a href='https://www.udemy.com/course/modern-react-front-to-back/'>
+          <a className='text-light' href='https://www.udemy.com/course/modern-react-front-to-back/'>
             {' '}
             React Front To Back
           </a>{' '}
           Udemy course by
           <strong>
-            <a href='https://traversymedia.com'> Brad Traversy</a>
+            <a className='text-light' href='https://traversymedia.com'> Brad Traversy</a>
           </strong>
           .
         </p>
-        <p className='text-lg '>
-          Version <span className='text-white'>1.0.0</span>
+        <p>
+          Version <span>1.0.0</span>
         </p>
-        <p className='text-lg '>
+        <p>
           Layout By:
-          <a className='text-white' href='https://twitter.com/hassibmoddasser'>
+          <a className='text-light' href='https://twitter.com/hassibmoddasser'>
             {' '}Hassib Moddasser
           </a>
         </p>
